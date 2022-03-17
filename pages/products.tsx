263d9534192bc326a5ab5481ba433d2e855ab778
Joseph Izang <josephizang@gmail.com>
2022-03-17 23:22:38 +0100

    add react-query library.

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
 import ProductList from '../components/products/ProductList'
 import { ProductShape } from './make-sale'
 
 export default function Products() {
+  const [products, setProducts] = useState<ProductShape[]>([])
+  const productQuery = useQuery('fetchProducts', () => {})
   return (
     <Layout>
       <ProductList products={products} />
     </Layout>
   )
+}
+function useQuery(arg0: string, arg1: () => void) {
+  throw new Error('Function not implemented.')
 }
